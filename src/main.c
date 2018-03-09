4779fc2e242c77cb69db9dc8d8858656d8223956
ablin <ablin@student.42.fr>
2018-03-10 00:45:06 +0100

    sorting and looking for parsing options

--- src/main.c
+++ src/main.c
@@ -1,45 +1,25 @@
 #include "../minilibx_macos/mlx.h"
 #include "../include/wire.h"
+#include "../include/get_next_line.h"
+#include <stdio.h>
 
-int		main()
+int		main(int ac, char **av)
 {
-	t_wire	wire;
-	t_img	img;
-	int x;
-	int y;
+//	t_wire	wire;
+	t_map	map;
 
-	x = 100;
-	y = 100;
-	img.bpp = 8;
-	img.size_l = 100;
-	img.endian = 1;
+	if (ac != 2)
+		return (0);
+	map.fd = open(av[1], O_RDONLY);
+	get_next_line(map.fd, &map.line);
+	printf("[%s]", map.line);
+	printf("%lu", ((ft_strlen(map.line) / 3) + ft_strlen(map.line) % 3));
+//	img.bpp = 8;
+//	img.size_l = 100;
+//	img.endian = 1;
 
-	wire.mlx = mlx_init();
-	wire.win = mlx_new_window(wire.mlx, 500, 500, "WireFrame");
-	wire.img = mlx_new_image(wire.mlx, 100, 100);
-	wire.addr = mlx_get_data_addr(wire.img, &img.bpp, &img.size_l, &img.endian);
-	mlx_pixel_put(wire.mlx, wire.win, 20, 20, 0x009900);
-	mlx_pixel_put(wire.mlx, wire.win, 20, 480, 0x009900);
-	mlx_pixel_put(wire.mlx, wire.win, 480, 20, 0x009900);
-	mlx_pixel_put(wire.mlx, wire.win, 480, 480, 0x009900);
-	while (x <= 401 && y < 401)
-	{
-		mlx_pixel_put(wire.mlx, wire.win, x, y, 0x990000);
-		x++;
-		if (x == 400)
-		{
-			x = 100;
-			y++;
-			while (y % 100 != 0 && y < 401)
-			{
-				mlx_pixel_put(wire.mlx, wire.win, x, y, 0x990000);
-				mlx_pixel_put(wire.mlx, wire.win, x+100, y, 0x990000);
-				mlx_pixel_put(wire.mlx, wire.win, x+200, y, 0x990000);
-				mlx_pixel_put(wire.mlx, wire.win, x+300, y, 0x990000);
-				y++;
-			}
-		}
-	}
-	mlx_loop(wire.mlx);
+	//wire.mlx = mlx_init();
+	//wire.win = mlx_new_window(wire.mlx, 500, 500, "WireFrame");
+	//mlx_loop(wire.mlx);
 	return (0);
 }

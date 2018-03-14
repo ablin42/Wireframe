42ce4d13b56fde0cd7e502b403029f9098a0c83a
ablin <ablin@student.42.fr>
2018-03-14 23:45:26 +0100

    i need to find how to interpret the values

--- src/main.c
+++ src/main.c
@@ -5,10 +5,22 @@
 
 t_map		ft_getvalues(t_map map)
 {
-	if (map.x == NULL)
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	while (map.line[i] != '\0')
 	{
-		ft_strlen(map.line);
+		//map.x[0][j] = map.line[i];
+		//j++;
+		if (ft_isdigit(map.line[i]) == 1)
+		{
+			ft_putchar(map.line[i]);
+		}
+		i++;
 	}
+		ft_putchar(map.line[i]);
 	return (map);
 }
 
@@ -16,14 +28,29 @@ int			main(int ac, char **av)
 {
 //	t_wire	wire;
 	t_map	map;
+	int		i;
+	int		j;
 
 	if (ac != 2)
 		return (0);
+	i = 0;
+	map.fd = open(av[1], O_RDONLY);
+	while (get_next_line(map.fd, &map.line) > 0)
+		i++;
+	close(map.fd);
+	j = i;
+	map.x = (int**)malloc(sizeof(map.x) * (i + 1));
+	while (j < i)
+	{
+		map.x[j] = (int*)malloc(sizeof(int *) * ((ft_strlen(map.line) / 3) + 1));
+	}
 	map.fd = open(av[1], O_RDONLY);
 	while (get_next_line(map.fd, &map.line) > 0)
 	{
+		map = ft_getvalues(map);
 		printf("[%s]\n", map.line);
 	}
+
 	//printf("%lu", ((ft_strlen(map.line) / 3) + ft_strlen(map.line) % 3));
 //	img.bpp = 8;
 //	img.size_l = 100;

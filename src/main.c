d9ed1974e0107a2ee41a1f3427ea4341336ad959
Andreas Blin <ablin@student.42.fr>
2018-03-15 02:04:33 +0100

    step by step

--- src/main.c
+++ src/main.c
@@ -3,7 +3,7 @@
 #include "../include/get_next_line.h"
 #include <stdio.h>
 
-t_map		ft_getvalues(t_map map)
+t_map		ft_getvalues(t_map map, int k)
 {
 	int		i;
 	int		j;
@@ -12,15 +12,20 @@ t_map		ft_getvalues(t_map map)
 	j = 0;
 	while (map.line[i] != '\0')
 	{
-		//map.x[0][j] = map.line[i];
-		//j++;
-		if (ft_isdigit(map.line[i]) == 1)
+		while (ft_isdigit(map.line[i]) == 1)
 		{
-			ft_putchar(map.line[i]);
+			map.x[k][j] += ft_atoi(&map.line[i]);
+			i++;
+			if (map.line[i] == '\0' || ft_isdigit(map.line[i]) == 0)
+			{
+				ft_putstr(ft_itoa(map.x[k][j]));
+				j++;
+				break;
+			}
 		}
 		i++;
 	}
-		ft_putchar(map.line[i]);
+	ft_putchar('\n');
 	return (map);
 }
 
@@ -30,25 +35,35 @@ int			main(int ac, char **av)
 	t_map	map;
 	int		i;
 	int		j;
+	int		k;
+	int		linesize;
 
 	if (ac != 2)
 		return (0);
 	i = 0;
+	j = 0;
+	linesize = 0;
+	k = 0;
 	map.fd = open(av[1], O_RDONLY);
 	while (get_next_line(map.fd, &map.line) > 0)
+	{
+		if (linesize == 0)
+			linesize = (ft_strlen(map.line) / 3) + 1;
 		i++;
+	}
 	close(map.fd);
-	j = i;
 	map.x = (int**)malloc(sizeof(map.x) * (i + 1));
 	while (j < i)
 	{
-		map.x[j] = (int*)malloc(sizeof(int *) * ((ft_strlen(map.line) / 3) + 1));
+		map.x[j] = (int*)malloc(sizeof(int *) * (linesize + 1));
+		j++;
 	}
 	map.fd = open(av[1], O_RDONLY);
 	while (get_next_line(map.fd, &map.line) > 0)
 	{
-		map = ft_getvalues(map);
-		printf("[%s]\n", map.line);
+		map = ft_getvalues(map, k);
+		k++;
+	//	printf("[%s]\n", map.line);
 	}
 
 	//printf("%lu", ((ft_strlen(map.line) / 3) + ft_strlen(map.line) % 3));
